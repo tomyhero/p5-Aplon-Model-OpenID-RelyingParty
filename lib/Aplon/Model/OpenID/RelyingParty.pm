5545d988f34ead9250d6f73fa39df05d04f97248
polocky <teranishi@livedoor.jp>
2011-04-29 20:30:55 +0900

    initial commit

---
+++ lib/Aplon/Model/OpenID/RelyingParty.pm
@@ -0,0 +1,32 @@
+package Aplon::Model::OpenID::RelyingParty;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Aplon::Model::OpenID::RelyingParty -
+
+=head1 SYNOPSIS
+
+  use Aplon::Model::OpenID::RelyingParty;
+
+=head1 DESCRIPTION
+
+Aplon::Model::OpenID::RelyingParty is
+
+=head1 AUTHOR
+
+Tomohiro Teranishi E<lt>tomohiro.teranishi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,4 @@
+use strict;
+use Test::More tests => 1;
+
+BEGIN { use_ok 'Aplon::Model::OpenID::RelyingParty' }
